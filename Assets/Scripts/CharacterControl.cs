159e5dc0f8999f5742288d5fbf506a954ae132a1
Ryan <ryansmorris@hotmail.com>
2020-09-24 15:40:51 +1000

    Revert "/"
    This reverts commit 724cbf56344dc4ea92d3e3ac030bfa39a84760ec.

--- Assets/Scripts/CharacterControl.cs
+++ Assets/Scripts/CharacterControl.cs
@@ -18,11 +18,8 @@ public class CharacterControl : MonoBehaviour
     private int jumpCount;
     Transform camerat;
     Animator anim;
-<<<<<<< HEAD
     public static bool aiming;
 
-=======
->>>>>>> parent of e409cf1... Update: Hub World UI elements constructed
 
     // Start is called before the first frame update
     void Start()
@@ -50,15 +47,8 @@ public class CharacterControl : MonoBehaviour
         if (Input.anyKey == false) {
             anim.Play("Normal Status", 0, 1);
         }
-<<<<<<< HEAD
         float targetRotation = Mathf.Atan2(Input.GetAxis("Horizontal"), Input.GetAxis("Vertical")) * Mathf.Rad2Deg + camerat.eulerAngles.y;  
     }  
-=======
-        float targetRotation = Mathf.Atan2(Input.GetAxis("Horizontal"), Input.GetAxis("Vertical")) * Mathf.Rad2Deg + camerat.eulerAngles.y;
-
-    }
-
->>>>>>> parent of e409cf1... Update: Hub World UI elements constructed
     void PlayerMove() {
         // defines horizontal and vertical controls
         float hor = Input.GetAxis("Horizontal");
---
+++ Assets/Scripts/Explosion.cs
@@ -0,0 +1,24 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Explosion : MonoBehaviour
+{
+    public float lifeTime = 4.0f;
+    public float elapsedTime;
+    // Start is called before the first frame update
+    void Start()
+    {
+        elapsedTime = 0.0f;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        elapsedTime += Time.deltaTime;
+
+        if (elapsedTime >= lifeTime) {
+            Destroy(this.gameObject);
+        }
+    }
+}
