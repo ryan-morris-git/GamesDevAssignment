85152c2947498f63a955903033e1067c8428ae05
Ryan Morris <52441725+ryan-morris-git@users.noreply.github.com>
2020-10-05 11:26:43 +1000

    ..

--- Assets/Scripts/Bullet.cs
+++ Assets/Scripts/Bullet.cs
@@ -6,7 +6,6 @@ public class Bullet : MonoBehaviour
 {
     public float lifeTime = 3.0f;
     public float elapsedTime;
-    public int damage = 20;
 
     // Start is called before the first frame update
     void Start()
